45787d8417cf531356cee18728c3548fbfdb4e6b
Andrew <njayaandrew@gmail.com>
2019-01-09 15:07:49 +0300

    [chore #163081872] a test to validate values passed to create question endpoint [finishes #163081872]

--- validators/validate_json.py
+++ validators/validate_json.py
@@ -8,5 +8,12 @@ class validate_json_values():
             return False     
         else:
             return True
+    """""""Class method checks if json value is actually an interger and returns a boolean True else false"""""""
+    @classmethod
+    def validate_json_integer_value(cls,json_value):
+        if type(json_value)==int:
+            return True    
+        else:
+            return False
             
         
