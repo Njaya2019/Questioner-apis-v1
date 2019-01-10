fc1f3f78cda89722a841005e5f2983fc4deacf51
Andrew <njayaandrew@gmail.com>
2019-01-10 16:43:53 +0300

    [bug #163116343] fixed a bug to upvote a question and downvote it by different users[finishes #163116343]

--- models/questions_model.py
+++ models/questions_model.py
@@ -2,10 +2,10 @@ import datetime
 
 """""""Initialized a global list to hold all questions"""""""
 questions_list=[]
-
+"""""""""makes votes variable a global variable"""""""""
+votes=0
 """""""A class model of questions"""""""
 class questionsmodel():
-    votes=0
     """""""Create instance variables of class questionsmodel"""""""
     def __init__(self,question_title,question_description,user_id,meetup_id):
         self.question_title=question_title
@@ -19,7 +19,7 @@ class questionsmodel():
         self.question_id=len(questions_list)+1
     """""""An intance method to ask a question and returnit's id,title and description"""""""
     def ask_question(self):
-        question_dict={"question_id":self.question_id,"question_title":self.question_title,"question_description":self.question_description,"user_id":self.user_id,"meetup_id":self.meetup_id,"date_created":self.date_asked,"votes":self.votes}
+        question_dict={"question_id":self.question_id,"question_title":self.question_title,"question_description":self.question_description,"user_id":self.user_id,"meetup_id":self.meetup_id,"date_created":self.date_asked,"votes":votes}
         questions_list.append(question_dict)
         return {"question_id":self.question_id,"question_title":self.question_title,"question_description":self.question_description}
     """""""A class method to vote upvote on a question by increamenting votes variable by 1"""""""
@@ -30,8 +30,10 @@ class questionsmodel():
         if not questions_list:
             return "Forbidden. The question doesn\'t exist"
         """""""gets the question and upvotes it"""""""
+        global votes
+        votes=votes+1
         question_upvote=questions_list[question_id-1]
-        question_upvote.update({"votes":cls.votes+1})
+        question_upvote.update({"votes":votes})
         return {"meetup_id":question_upvote['meetup_id'],"question_title":question_upvote['question_title'],"question_description":question_upvote['question_description'],"votes":question_upvote['votes']}
     """""""A class method to down vote on a question by decreamenting votes variable by 1"""""""
     @classmethod
@@ -41,6 +43,8 @@ class questionsmodel():
         if not questions_list:
             return "Forbidden. The question doesn\'t exist"
         """""""gets the question and downvotes it"""""""
+        global votes
+        votes=votes-1
         question_downvote=questions_list[question_id-1]
-        question_downvote.update({"votes":cls.votes+1})
+        question_downvote.update({"votes":votes})
         return {"meetup_id":question_downvote['meetup_id'],"question_title":question_downvote['question_title'],"question_description":question_downvote['question_description'],"votes":question_downvote['votes']}
