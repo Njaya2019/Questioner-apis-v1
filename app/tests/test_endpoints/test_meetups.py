0699f859835c811ab9ce06a6df5d1745052ea657
Andrew <njayaandrew@gmail.com>
2019-01-11 17:56:30 +0300

    [chore #163148131] Organised tests in classes. Having tests as instance methods of these classes [finishes #163148131]

--- app/tests/test_endpoints/test_meetups.py
+++ app/tests/test_endpoints/test_meetups.py
@@ -12,50 +12,61 @@ def cli_ent():
     client=app.test_client()
     return client
 
-"""""""This tests an endpoint to create a meetup and uses test client of flask app"""""""
-def test_createmeetup(cli_ent):
-    now=datetime.datetime.now()
-    now=now.strftime("%Y-%m-%d %H:%M")
-    response=cli_ent.post('/api/v1/admin/createmeetup',data=json.dumps(dict(meetup_title="python programming for beginners",meetup_description="We started this to help each other.Regardless of your experince just join us share and learn.Welcome!",location="Mombasa,Kenya",date_created=now)),content_type="application/json")
-    data=json.loads(response.data)
-    assert response.status_code==201
-    assert "python programming for beginners"==data["meeeup_created"]["meetup_title"]
+"""""""Class to test an endpoint that creates a meetup.It has instance methods"""""""
+class TestCreateMeetUps():
+    
+    """""""instance method to test create a meetup"""""""
+    """""""This tests an endpoint to create a meetup and uses test client of flask app"""""""
+    def test_createmeetup(self,cli_ent):
+        now=datetime.datetime.now()
+        now=now.strftime("%Y-%m-%d %H:%M")
+        response=cli_ent.post('/api/v1/admin/createmeetup',data=json.dumps(dict(meetup_title="python programming for beginners",meetup_description="We started this to help each other.Regardless of your experince just join us share and learn.Welcome!",location="Mombasa,Kenya",date_created=now)),content_type="application/json")
+        data=json.loads(response.data)
+        assert response.status_code==201
+        assert "python programming for beginners"==data["meeeup_created"]["meetup_title"]
 
-"""""""This tests if one of the values of meet up information wasn't provided"""""""
-def test_createmeetup_empty_value(cli_ent):
-    now=datetime.datetime.now()
-    now=now.strftime("%Y-%m-%d %H:%M")
-    response=cli_ent.post('/api/v1/admin/createmeetup',data=json.dumps(dict(meetup_title="python programming for beginners",meetup_description=None,location="Mombasa,Kenya",date_created=now)),content_type="application/json")
-    data=json.loads(response.data)
-    assert response.status_code==422
-    assert "Please fill all the meet up information" in data["error_msg"]
+    """""""Instance method to test empty posted values"""""""
+    """""""This tests if one of the values of meet up information wasn't provided"""""""
+    def test_createmeetup_empty_value(self,cli_ent):
+        now=datetime.datetime.now()
+        now=now.strftime("%Y-%m-%d %H:%M")
+        response=cli_ent.post('/api/v1/admin/createmeetup',data=json.dumps(dict(meetup_title="python programming for beginners",meetup_description=None,location="Mombasa,Kenya",date_created=now)),content_type="application/json")
+        data=json.loads(response.data)
+        assert response.status_code==422
+        assert "Please fill all the meet up information" in data["error_msg"]
 
-"""""""This tests if the values are invalid. An integer instead of a string."""""""
-def test_createmeetup_invalid_value(cli_ent):
-    now=datetime.datetime.now()
-    now=now.strftime("%Y-%m-%d %H:%M")
-    response=cli_ent.post('/api/v1/admin/createmeetup',data=json.dumps(dict(meetup_title="python programming for beginners",meetup_description=1234,location="Mombasa,Kenya",date_created=now)),content_type="application/json")
-    data=json.loads(response.data)
-    assert response.status_code==400
-    assert "Please provide valid data" in data["error_msg"]
+    """""""Instance method to test posted invalid values"""""""
+    """""""This tests if the values are invalid. An integer instead of a string."""""""
+    def test_createmeetup_invalid_value(self,cli_ent):
+        now=datetime.datetime.now()
+        now=now.strftime("%Y-%m-%d %H:%M")
+        response=cli_ent.post('/api/v1/admin/createmeetup',data=json.dumps(dict(meetup_title="python programming for beginners",meetup_description=1234,location="Mombasa,Kenya",date_created=now)),content_type="application/json")
+        data=json.loads(response.data)
+        assert response.status_code==400
+        assert "Please provide valid data" in data["error_msg"]
 
-"""""""A test to get a specific meetup record"""""""
-def test_get_a_meetup_record(cli_ent):
-    response=cli_ent.get('/api/v1/admin/meetups/'+str(1))
-    data=json.loads(response.data)
-    assert response.status_code==200
-    assert data["meetup_record"]["meetup_title"]=="python programming for beginners"
 
-"""""""Tests if the meetup wasn't found"""""""
-def test_get_a_meetup_record_notfound(cli_ent):
-    response=cli_ent.get('/api/v1/admin/meetups/'+str(2))
-    data=json.loads(response.data)
-    assert response.status_code==404
-    assert 'The meetup record wasn\'t found' in data["error_msg"]
+"""""""Class to test endpoints that that get meetups.It has instance methods"""""""
 
-"""""""Tests if the enpoint can get all meet ups"""""""
-def test_get_all_meetups(cli_ent):
-    response=cli_ent.get('/api/v1/admin/meetups')
-    data=json.loads(response.data)
-    assert response.status_code==200
-    assert data=={'Meetups':meetups_list}
+class TestGetMeetUps():
+
+    """""""A test to get a specific meetup record"""""""
+    def test_get_a_meetup_record(self,cli_ent):
+        response=cli_ent.get('/api/v1/admin/meetups/'+str(1))
+        data=json.loads(response.data)
+        assert response.status_code==200
+        assert data["meetup_record"]["meetup_title"]=="python programming for beginners"
+
+    """""""Tests if the meetup wasn't found"""""""
+    def test_get_a_meetup_record_notfound(self,cli_ent):
+        response=cli_ent.get('/api/v1/admin/meetups/'+str(2))
+        data=json.loads(response.data)
+        assert response.status_code==404
+        assert 'The meetup record wasn\'t found' in data["error_msg"]
+
+    """""""Tests if the enpoint can get all meet ups"""""""
+    def test_get_all_meetups(self,cli_ent):
+        response=cli_ent.get('/api/v1/admin/meetups')
+        data=json.loads(response.data)
+        assert response.status_code==200
+        assert data=={'Meetups':meetups_list}
