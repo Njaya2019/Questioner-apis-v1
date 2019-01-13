0f948d9f2bf99fae8e1fa1ee4b6a15624b89e7ff
Andrew <njayaandrew@gmail.com>
2019-01-13 10:36:01 +0300

    [bug #163167376] added a field for a user to choose the type of account he/she wants.changed http status codes for invalid values to bad request.Added a functionality to validate email and wrote tests [finishes #163167376]

--- app/tests/test_endpoints/test_users_auth.py
+++ app/tests/test_endpoints/test_users_auth.py
@@ -17,7 +17,7 @@ class TestsUserRegistration():
     
     """""""This instance method test for successful registration of users""""""" 
     def test_register_user(self,cli_ent):
-        response=cli_ent.post('/api/v1/registration',data=json.dumps(dict(user_firstname="Juma",user_secondname="Masha",user_gender="Male",user_email="jumamasha@gmail.com",isAdmin="Admin",password="1234",confirm_password="1234")),content_type="application/json")
+        response=cli_ent.post('/api/v1/registration',data=json.dumps(dict(user_firstname="Juma",user_secondname="Masha",user_gender="Male",user_email="jumamasha@gmail.com",account_type="Admin",password="1234",confirm_password="1234")),content_type="application/json")
         data=json.loads(response.data)
         assert response.status_code==201
         """""""This test if the enpoint registers a user and returns a user id,first name and second name"""""""
@@ -28,18 +28,23 @@ class TestsUserRegistration():
         assert True==data["user"]["isAdmin"]
     """""""This intance method tests if user values passed are empty"""""""
     def test_register_user_empty_values(self,cli_ent):
-        response=cli_ent.post('/api/v1/registration',data=json.dumps(dict(user_firstname=None,user_secondname=None,user_gender="Male",user_email="jumamasha@gmail.com",isAdmin="Admin",password="1234",confirm_password="1234")),content_type="application/json")
+        response=cli_ent.post('/api/v1/registration',data=json.dumps(dict(user_firstname=None,user_secondname=None,user_gender="Male",user_email="jumamasha@gmail.com",account_type="Admin",password="1234",confirm_password="1234")),content_type="application/json")
         data=json.loads(response.data)
-        assert response.status_code==422
-        assert "All user's information is required to register" in data["error"]
+        assert response.status_code==400
+        assert "All user's information is required to register" in data["error_msg"]
 
     """""""This instance method tests if the user values passed are valid"""""""
     def test_register_user_invalid_values(self,cli_ent):
-        response=cli_ent.post('/api/v1/registration',data=json.dumps(dict(user_firstname=1,user_secondname=1234,user_gender="Male",user_email="jumamasha@gmail.com",isAdmin="Admin",password="1234",confirm_password="1234")),content_type="application/json")
+        response=cli_ent.post('/api/v1/registration',data=json.dumps(dict(user_firstname=1,user_secondname=1234,user_gender="Male",user_email="jumamasha@gmail.com",account_type="Admin",password="1234",confirm_password="1234")),content_type="application/json")
         data=json.loads(response.data)
         assert response.status_code==400
-        assert "Please provide user valid values" in data["error"]
+        assert "Please provide user valid values" in data["error_msg"]
         assert 400==data["status"]
-    
+    def test_register_user_invalid_email(self,cli_ent):
+        response=cli_ent.post('/api/v1/registration',data=json.dumps(dict(user_firstname="Willy",user_secondname="Mzae",user_gender="Male",user_email="willymzaegmail.com",account_type="Admin",password="1234",confirm_password="1234")),content_type="application/json")
+        data=json.loads(response.data)
+        assert response.status_code==422
+        assert "Please provide a valid email" in data["error_msg"]
+        assert 400==data["status"]
 
 
--- models/user_model.py
+++ models/user_model.py
@@ -5,12 +5,12 @@ users_list=[]
 
 """""""A class that has instance models and class methods"""""""
 class users_model():
-    def __init__(self,first_name,second_name,gender,email,password,confirm_pwd):
+    def __init__(self,first_name,second_name,gender,email,account_type,password,confirm_pwd):
         self.first_name=first_name
         self.second_name=second_name
-        self.isAdmin=True
         self.gender=gender
         self.email=email
+        self.account_type=account_type
         self.password=password
         self.confirm_pwd=confirm_pwd
         self.user_id=len(users_list)+1
@@ -21,9 +21,19 @@ class users_model():
         for user in users_list:
             if user['email']==self.email:
                 return "The email already exists. Choose another email"
+        account=users_model.acconttype_to_boolean(self.account_type)
         hashed_password=generate_password_hash(self.password)
-        users_dict={"user_id":self.user_id,'user_firstname':self.first_name,'user_secondname':self.second_name,'isAdmin':self.isAdmin,'user_gender':self.gender,'email':self.email,'password':hashed_password}
+        users_dict={"user_id":self.user_id,'user_firstname':self.first_name,'user_secondname':self.second_name,'isAdmin':account,'user_gender':self.gender,'email':self.email,'password':hashed_password}
         users_list.append(users_dict)
         return users_dict
+    """""""A class method to convert user account type to boolean"""""""
+    @classmethod
+    def acconttype_to_boolean(cls,accounttype):
+        if accounttype=='Admin':
+            isAdmin=True
+            return isAdmin
+        else:
+            return False
+            
         
             
